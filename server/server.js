3e77084ab42be9ea94034bc4ef52fbe33dabb013
Matt Halvorson <halvorson34.mh@gmail.com>
2020-04-03 16:22:52 -0500

    added server functions

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const pg = require("pg");
+
+const app = express();
+const Pool = pg.Pool;
+const PORT = process.env.PORT || 5000;
+
+const pool = new Pool({
+  database: "weekend-to-do-app",
+  host: "localhost",
+  port: 5432,
+  max: 10,
+  idelTimeoutMills: 3000,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("server/public"));
+
+app.listen(Port, () => {
+  console.log("Running server on PORT:", PORT);
+});
