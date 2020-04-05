1636be99d702f5ed1e35fe4c20e5f042906b803b
Matt Halvorson <halvorson34.mh@gmail.com>
2020-04-04 19:04:58 -0500

    set up get and post functions in list.router.js

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -2,4 +2,37 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
+listRouter.get("/", (req, res) => {
+  const queryText = `SELECT * FROM "list";`;
+  pool
+    .query(queryText)
+    .then((responseDB) => {
+      const dbRows = responseDB.rows;
+      console.log(dbRows);
+      res.send(dbRows);
+    })
+    .catch((err) => {
+      console.log("Error with GET:", err);
+      res.sendStatus(500);
+    });
+});
+
+listRouter.post("/", (req, res) => {
+  const dataSentFromClient = req.body;
+
+  const queryText = `INSERT INTO "list" ("task", "complete")
+        VALUES ($1, $2);`;
+
+  pool
+    .query(queryText, [dataSentFromClient.task, dataSentFromClient.complete])
+    .then((responseDb) => {
+      console.log(responseDb);
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log("Error with POST:", err);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
